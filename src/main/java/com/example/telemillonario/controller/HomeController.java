7dedcd462c18e425d985e379ff442b05ad88664f
Alonso Rosales <a20191566@pucp.edu.pe>
2022-04-15 21:08:13 -0500

    admin-sedes

--- src/main/java/com/example/telemillonario/controller/HomeController.java
+++ src/main/java/com/example/telemillonario/controller/HomeController.java
@@ -11,4 +11,7 @@ public class HomeController {
     public String inicio(){
         return "Administrador/Sede/listaSedes";
     }
+
+    /*Pagina para sacar los iconos
+    https://iconos8.es/icons/set/edit*/
 }
