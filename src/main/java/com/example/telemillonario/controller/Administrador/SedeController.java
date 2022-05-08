cdc2f688536fc511ccfda97c4acf95eb84476d03
Percy De La Rosa <a20192265@pucp.edu.pe>
2022-05-08 00:37:28 -0500

    Se cambiaron las rutas de admin de "/url" a "/admin/url"

--- src/main/java/com/example/telemillonario/controller/Administrador/SedeController.java
+++ src/main/java/com/example/telemillonario/controller/Administrador/SedeController.java
@@ -1,12 +1,17 @@
 package com.example.telemillonario.controller.Administrador;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/Sede")
+@RequestMapping( value = {"/admin","/admin/sedes"})
 public class SedeController {
 
+    @GetMapping(value = {"", "/","/lista"})
+    public String paginaPrincipal(){
+        return "Administrador/Sede/listaSedes";
+    }
 
 
 }
