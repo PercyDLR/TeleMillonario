6a75415e348623d11befc4f30f0dd4c7f6aff8dd
Percy De La Rosa <a20192265@pucp.edu.pe>
2022-05-24 00:12:22 -0500

    Retoques visuales

--- src/main/java/com/example/telemillonario/controller/Usuario/UsuarioController.java
+++ src/main/java/com/example/telemillonario/controller/Usuario/UsuarioController.java
@@ -2,12 +2,10 @@ package com.example.telemillonario.controller.Usuario;
 
 import com.example.telemillonario.entity.Persona;
 import com.example.telemillonario.repository.PersonaRepository;
-import com.example.telemillonario.validation.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +13,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.util.Objects;
 
 @Controller
 public class UsuarioController {
