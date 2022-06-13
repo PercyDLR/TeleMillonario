6cd285252ddfb91e6dac69ce149bf740d183f769
Alonso Rosales <a20191566@pucp.edu.pe>
2022-06-13 12:50:24 -0500

    envio de correo al finalizar compra, borrado de compra en historial

--- src/main/java/com/example/telemillonario/repository/PagoRepository.java
+++ src/main/java/com/example/telemillonario/repository/PagoRepository.java
@@ -3,11 +3,14 @@ package com.example.telemillonario.repository;
 import com.example.telemillonario.entity.Compra;
 import com.example.telemillonario.entity.Pago;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PagoRepository extends JpaRepository<Pago,Integer> {
 
+    @Query(nativeQuery = true,value = "SELECT * FROM pago WHERE idcompra = ?1")
+    Pago encontrarPagoPorIdCompra(int idCompra);
 
 
 }
