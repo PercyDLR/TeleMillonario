fca11ae497067c62f029aa306e3cc291cf3c6234
Alonso Rosales <a20191566@pucp.edu.pe>
2022-06-14 16:51:57 -0500

    fix

--- src/main/java/com/example/telemillonario/controller/LoginController.java
+++ src/main/java/com/example/telemillonario/controller/LoginController.java
@@ -179,11 +179,11 @@ public class LoginController {
 
 
         DatosAPI datosPersona = DniAPI.consulta(usuario.getDni());
-        /*
+        boolean errDNI = true;
         //Verificamos si existe el dni
         if(datosPersona.getSuccess().equalsIgnoreCase("true")){
             //pasamos a mayuscula los nombres apellidos de la persona que se registro
-            String nombresUpperCase = usuario.getNombres().toUpperCase();
+            /*String nombresUpperCase = usuario.getNombres().toUpperCase();
             String apellidosUpperCase = usuario.getApellidos().toUpperCase();
             String nombresApellidosUpperCase = nombresUpperCase + " " + apellidosUpperCase;
 
@@ -192,16 +192,17 @@ public class LoginController {
 
             if(!nombresApellidosUpperCase.equals(nombresApellidosAPI)){
                 model.addAttribute("errDniNoCorrespondePersona", 1);
-            }
+            }*/
+            errDNI = false;
         }
         else{
-            model.addAttribute("errDni", 1);
-        }*/
-        if(!datosPersona.getSuccess().equalsIgnoreCase("true")){
-            model.addAttribute("errDni", 1);
+            model.addAttribute("errDni", errDNI);
         }
+        /*if(!datosPersona.getSuccess().equalsIgnoreCase("true")){
+            model.addAttribute("errDni", errDNI);
+        }*/
 
-        if(bindingResult.hasErrors() || coincidencias || errorRecontrasenia || errorNacimiento){
+        if(bindingResult.hasErrors() || coincidencias || errorRecontrasenia || errorNacimiento || errDNI){
             if (google != null && google == 1) {
                 model.addAttribute("google", 1);
             }
