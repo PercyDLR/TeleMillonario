6ae5cdb1d045d19aec7ac7dbd52f11614477e56b
avizcarra <a20181454@pucp.edu.pe>
2022-06-14 01:24:30 -0500

    Implementacion de servicio de QR y de validacion de tarjeta con dtos y daos

---
+++ src/main/java/com/example/telemillonario/dto/ValidacionTarjetaDto.java
@@ -0,0 +1,23 @@
+package com.example.telemillonario.dto;
+
+public class ValidacionTarjetaDto {
+
+    private String estado;
+    private String msg;
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
