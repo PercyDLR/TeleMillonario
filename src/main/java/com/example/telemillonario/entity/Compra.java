53b93da24d37584d46d848b496496b10f999d5e5
Alonso Rosales <a20191566@pucp.edu.pe>
2022-06-14 20:48:05 -0500

    fix

--- src/main/java/com/example/telemillonario/entity/Compra.java
+++ src/main/java/com/example/telemillonario/entity/Compra.java
@@ -3,12 +3,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "compra")
 @Getter
 @Setter
-public class Compra {
+
+public class Compra implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
