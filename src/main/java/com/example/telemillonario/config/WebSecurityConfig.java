7585c472628a9445c9be25c56d9275af3f3df836
Percy De La Rosa <a20192265@pucp.edu.pe>
2022-07-11 21:10:21 -0500

    Cambio ruta /historialPrueba por /historial

--- src/main/java/com/example/telemillonario/config/WebSecurityConfig.java
+++ src/main/java/com/example/telemillonario/config/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasAuthority("Administrador")
                 .antMatchers("/operador/**").hasAuthority("Operador")
-                .antMatchers("/calificarObra", "/historialPrueba").hasAuthority("Usuario")
+                .antMatchers("/calificarObra", "/historial").hasAuthority("Usuario")
                 .antMatchers("/perfil/**").hasAnyAuthority("Operador","Usuario","ROLE_USER")
                 .antMatchers("/","/cartelera/*","/sedes/*","/actores/*","/directores/*").access("hasAuthority('Usuario') || isAnonymous() || hasAuthority('ROLE_USER')")
                 .anyRequest().permitAll().and()
